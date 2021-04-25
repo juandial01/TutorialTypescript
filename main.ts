7f7fb5a00e666cd2a8c39d3e61accfd37cc2ae0b
Juan Diego Alzate <jd.alzate253@uniandes.edu.co>
2021-04-24 19:27:00 -0500

    Operador => y la función filter

--- main.ts
+++ main.ts
@@ -12,10 +12,30 @@ console.log(ap.cursos)
 let aprendizTable: HTMLElement = document.getElementById("aprendiz")!;
 let estadisticasTable: HTMLElement = document.getElementById("estadisticas")!;
 let cursosTable: HTMLElement = document.getElementById("cursos")!;
+let btnFiltro: HTMLElement = document.getElementById("boton-filtro")!;
+let textoBusqueda: HTMLInputElement = <HTMLInputElement>document.getElementById("texto-busqueda")!;
+
+btnFiltro.onclick = () => {
+    let text:string = textoBusqueda.value;
+    text = (text==null)?"":text;
+    cursosTable.getElementsByTagName("tbody")[0].remove();
+    let cursosFiltrados: Curso[] = ap.cursos.filter(c => c.nombre.match(text));
+    mostrarCursosAprendiz(cursosFiltrados);
+};
 
 mostrarDatosAprendiz(ap);
 mostrarEstadisticas(ap);
-mostrarCursosAprendiz(ap);
+mostrarCursosAprendiz(ap.cursos);
+
+/* function filtrarPorNombre(): void{
+    let text:string = textoBusqueda.value;
+    text = (text==null)?"":text;
+    cursosTable.getElementsByTagName("tbody")[0].remove();
+    let cursosFiltrados: Curso[] = ap.cursos.filter(function(c){return c.nombre.match(text);})
+    mostrarCursosAprendiz(cursosFiltrados);
+} */
+
+
 
 function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
     let tbodyAprendiz = document.createElement("tbody");
@@ -30,13 +50,13 @@ function mostrarDatosAprendiz(aprendiz: Aprendiz):void{
 function mostrarEstadisticas(aprendiz: Aprendiz):void{    
     let numeroCertificados: number = aprendiz.darCursosCertificados();
     let trElement: HTMLElement = document.createElement("tr");
-    trElement.innerHTML = `<td><b>Cursos certificdados</b></td><td>${numeroCertificados}</td>`;
+    trElement.innerHTML = `<td><b>Cursos certificados</b></td><td>${numeroCertificados}</td>`;
     estadisticasTable.appendChild(trElement); 
 }
 
-function mostrarCursosAprendiz(aprendiz: Aprendiz):void{    
+function mostrarCursosAprendiz(cursos: Curso[]):void{    
     let cursosTbody: HTMLElement = document.createElement("tbody");
-    for (let curso of aprendiz.cursos)
+    for (let curso of cursos)
     {
         let trElement:HTMLElement = document.createElement("tr");
         trElement.innerHTML=`<td>${curso.nombre}</td>
